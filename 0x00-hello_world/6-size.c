1d3e53ac27b425ad62e814e28a89ed2be410175b
Hassan Ismail <hassannasr6218@gmail.com>
2023-03-16 01:22:34 -0700

    let's have another tyr for 5 and 6 with style

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,11 +1,17 @@
 #include <stdio.h>
 
-int main()
+/**
+ * main - entry point
+ *
+ * Return : Always 0 (success)
+ */
+
+int main(void)
 {
 	printf("Size of a char: %i byte(s)\n", sizeof(char));
 	printf("Size of an int: %i byte(s)\n", sizeof(int));
 	printf("Size of a long int: %i byte(s)\n", sizeof(long int));
 	printf("Size of a long long: %i byte(s)\n", sizeof(long long));
 	printf("Size of a float: %i byte(s)\n", sizeof(float));
-	return 0;
+	return (0);
 }
