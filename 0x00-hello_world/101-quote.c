bc30576b5eb3be2e1ecc352b143dd8c76c7930e5
Hassan Ismail <hassannasr6218@gmail.com>
2023-03-16 02:24:00 -0700

    advanced one added

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+
+
+/*
+ * main - entry point
+ *
+ * Return: Always 1 (success)
+ */
+int main(void)
+{
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", strlen("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"));
+	return 1;
+}
