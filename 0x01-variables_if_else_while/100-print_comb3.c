c4c51ac439320e9dd075f4a03677bc0791b97e95
Hassan Ismail <hassannasr6218@gmail.com>
2023-03-17 15:11:52 -0700

    adv1

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -6,5 +6,23 @@
  */
 int main(void)
 {
+	int digit1 = 48, digit2 =48;
+
+	for (digit1 = 48; digit1 < 58; digit1++)
+	{
+		for (digit2 = digit1 + 1; digit2 < 58; digit2++)
+		{
+			putchar(digit1);
+			putchar(digit2);
+			if (digit1 == 56 && digit2 == 57)
+			{
+				continue;
+			}
+			putchar(44);
+			putchar(32);
+			
+		}
+	}
+	putchar('\n');
 	return (0);
 }
