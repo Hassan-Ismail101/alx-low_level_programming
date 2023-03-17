9d26e7f1f65a37223966627cca6bbb3c9a94c450
Hassan Ismail <hassannasr6218@gmail.com>
2023-03-17 14:35:47 -0700

    comb1

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -6,5 +6,18 @@
  */
 int main(void)
 {
+	char counter = '0';
+
+	while (counter <= '9')
+	{
+		putchar(counter);
+		if (counter != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		counter++;
+	}
+	putchar('\n');
 	return (0);
 }
