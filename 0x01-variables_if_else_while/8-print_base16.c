7f4772a405492090c1c4f03cfff253f9f8eabb8a
Hassan Ismail <hassannasr6218@gmail.com>
2023-03-17 14:30:15 -0700

    base 16

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -6,5 +6,17 @@
  */
 int main(void)
 {
+	char counter = '0';
+
+	while (counter <= '9')
+	{
+		putchar(counter++);
+	}
+	counter = 'a';
+	while (counter <= 'f')
+	{
+		putchar(counter++);
+	}
+	putchar('\n');
 	return (0);
 }
