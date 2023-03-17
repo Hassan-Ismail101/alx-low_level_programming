dac3efa69680c38e026fb48b44d682d457985365
Hassan Ismail <hassannasr6218@gmail.com>
2023-03-17 14:04:29 -0700

    alpha1

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char counter = 'a';
+	while(counter <= 'z')
+	{
+		putchar(counter++);
+	}
+	putchar('\n');
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	return (0);
+}
