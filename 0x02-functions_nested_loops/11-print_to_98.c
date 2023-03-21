6c43360d93440618913c0c73bf9609dbe849f609
Hassan Ismail <hassannasr6218@gmail.com>
2023-03-21 04:18:51 -0700

    update of num11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -14,13 +14,16 @@ void print_to_98(int n)
 		{
 			printf("%d", counter);
 			if (counter != 98)
-				printf(", ");
+				_putchar(',');
+				_putchar(' ');
 		}
 	else
 		for (counter = n; counter >= 98; counter--)
 		{
 			printf("%d", counter);
 			if (counter != 98)
-				printf(", ");
+				_putchar(',');
+				_putchar(' ');
 		}
+	_putchar('\n');
 }
