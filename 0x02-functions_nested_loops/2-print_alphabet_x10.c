47d03fe1b32717c3f8b227e0c0c87b8742f69992
Hassan Ismail <hassannasr6218@gmail.com>
2023-03-21 03:36:17 -0700

    it wasn't joined

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,5 +1,16 @@
 #include "main.h"
 /**
+ * print_alpha - write the alpha letters
+ */
+static void print_alpha(void)
+{
+	char letter;
+
+	for(letter = 'a'; letter <= 'z'; letter++)
+		_putchar(letter);
+	_putchar('\n');
+}
+/**
  * print_alphabet_x10 - write print_alphabet (10) times
  */
 void print_alphabet_x10(void)
@@ -7,5 +18,5 @@ void print_alphabet_x10(void)
 	char counter;
 
 	for (counter = 1; counter <= 10; counter++)
-		print_alphabet();
+		print_alpha();
 }
