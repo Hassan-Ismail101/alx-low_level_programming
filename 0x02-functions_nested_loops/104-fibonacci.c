c03b6f0c38769be37d8e00b7921a2c3f3a3d927f
Hassan Ismail <hassannasr6218@gmail.com>
2023-03-21 07:57:07 -0700

    try for fibonacci3

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_fibonacci - write the @n times of fibonacci numbers
+ * @n: the number of counts
+ */
+void print_fibonacci(int n)
+{
+	unsigned long int temp1;
+	unsigned long int temp2;
+	unsigned long int currentValue;
+
+	temp2 = 0;
+	temp1 = 1;
+	while (n--)
+	{
+		currentValue = temp1 + temp2;
+		if (n != 0)
+			printf("%li, ", currentValue);
+		else
+			printf("%li\n", currentValue);
+		temp2 = temp1;
+		temp1 = currentValue;
+	}
+}
+/**
+ * main - entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	print_fibonacci(98);
+	return (0);
+}
