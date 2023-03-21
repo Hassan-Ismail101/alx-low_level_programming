207e8a7cf88317c40cf19074a3c76850a38a57e9
Hassan Ismail <hassannasr6218@gmail.com>
2023-03-21 10:43:25 -0700

    i thing it is the final

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -9,8 +9,7 @@ void print_fibonacci(int n)
 	unsigned int temp1_1, temp1_2, temp1_3, CV_1, part;
 	unsigned int temp2_1, temp2_2, temp2_3, CV_2, CV_3;
 
-	part = 100000000;
-	temp1_1 = 1, temp1_2 = 0, temp1_3 = 0;
+	part = 100000000, temp1_1 = 1, temp1_2 = 0, temp1_3 = 0;
 	temp2_1 = 0, temp2_2 = 0, temp2_3 = 0;
 	while (n--)
 	{
@@ -21,27 +20,23 @@ void print_fibonacci(int n)
 		CV_2 %= part;
 		if (n == 0)
 		{
-			printf("%i%i%i\n", CV_3, CV_2, CV_1);
+			printf("%i%.2i%.2i\n", CV_3, CV_2, CV_1);
 			break;
 		}
 		if (CV_3 > 0)
 		{
-			printf("%i%i%i, ", CV_3, CV_2, CV_1);
+			printf("%i%.2i%.2i, ", CV_3, CV_2, CV_1);
 		}
 		else if (CV_2 > 0)
 		{
-			printf("%i%i, ", CV_2, CV_1);
+			printf("%i%.2i, ", CV_2, CV_1);
 		}
 		else
 		{
 			printf("%i, ", CV_1);
 		}
-		temp2_1 = temp1_1;
-		temp2_2 = temp1_2;
-		temp2_3 = temp1_3;
-		temp1_1 = CV_1;
-		temp1_2 = CV_2;
-		temp1_3 = CV_3;
+		temp2_1 = temp1_1, temp2_2 = temp1_2, temp2_3 = temp1_3;
+		temp1_1 = CV_1, temp1_2 = CV_2, temp1_3 = CV_3;
 	}
 }
 /**
--- 0x02-functions_nested_loops/testing.c
+++ 0x02-functions_nested_loops/testing.c
@@ -1,11 +1,51 @@
+#include <stdio.h>
 #include "main.h"
+/**
+ * print_fibonacci - write the @n times of fibonacci numbers
+ * @n: the number of counts
+ */
+void print_fibonacci(int n)
+{
+	unsigned int temp1_1, temp1_2, temp1_3, CV_1, part;
+	unsigned int temp2_1, temp2_2, temp2_3, CV_2, CV_3;
+
+	part = 100, temp1_1 = 1, temp1_2 = 0, temp1_3 = 0;
+	temp2_1 = 0, temp2_2 = 0, temp2_3 = 0;
+	while (n--)
+	{
+		CV_1 = temp1_1 + temp2_1;
+		CV_2 = (temp1_2 + temp2_2) + (CV_1 / part);
+		CV_3 = (temp1_3 + temp2_3) + ((CV_2 / part));
+		CV_1 %= part;
+		CV_2 %= part;
+		if (n == 0)
+		{
+			printf("%i%.2i%.2i\n", CV_3, CV_2, CV_1);
+			break;
+		}
+		if (CV_3 > 0)
+		{
+			printf("%i%.2i%.2i, ", CV_3, CV_2, CV_1);
+		}
+		else if (CV_2 > 0)
+		{
+			printf("%i%.2i, ", CV_2, CV_1);
+		}
+		else
+		{
+			printf("%i, ", CV_1);
+		}
+		temp2_1 = temp1_1, temp2_2 = temp1_2, temp2_3 = temp1_3;
+		temp1_1 = CV_1, temp1_2 = CV_2, temp1_3 = CV_3;
+	}
+}
+/**
+ * main - entry point
+ *
+ * Return: Always 0 (success)
+ */
 int main(void)
 {
-	print_times_table(3);
-	_putchar('\n');
-	print_times_table(5);
-        _putchar('\n');
-	print_times_table(15);
-        _putchar('\n');
+	print_fibonacci(30);
 	return (0);
 }
