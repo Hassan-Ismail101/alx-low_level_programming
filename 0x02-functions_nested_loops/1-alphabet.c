dac9c655b055bc2c9a957616ac1403f107f47c4c
Hassan Ismail <hassannasr6218@gmail.com>
2023-03-21 03:18:57 -0700

    1- alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,15 +1,5 @@
 #include "main.h"
-/**
- * _putchar - writes the char to stdout
- * @c: The character to print
- *
- * Return: On success 1
- * On error, -1 is returned
- */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
+
 /**
  * print_alphabet - writes the alphabet in lowercase
  */
