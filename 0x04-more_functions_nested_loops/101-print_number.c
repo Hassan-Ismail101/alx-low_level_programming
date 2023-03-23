ce1343b3f34039f46a2da9ab2bf45b88cdc065d6
Hassan Ismail <hassannasr6218@gmail.com>
2023-03-23 16:49:51 -0700

    adv2

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * print_number - print the number with _putchar
@@ -7,29 +8,23 @@
 
 void print_number(int n)
 {
-	if (n != 0)
-	{
-		int flag;
-		int part = 1000000000;
+	int flag;
+	int part = 1000000000;
 
-		flag = 0;
-		if (n < 0)
-		{
-			_putchar('-');
-			n *= -1;
-		}
-		while (part)
-		{
-			if ((n / part) && (flag != 1))
-				flag = 1;
-			if (flag)
-			{
-				_putchar((n / part) + '0');
-			}
-			n %= part;
-			part /= 10;
-		}
+	flag = 0;
+	if (n < 0)
+	{
+		_putchar('-');
+		n *= -1;
 	}
-	else
-		_putchar('0');
+	while (part >= 10)
+	{
+		if ((n / part) && (flag != 1))
+			flag = 1;
+		if (flag)
+			_putchar((n / part) + '0');
+		n %= part;
+		part /= 10;
+	}
+	_putchar(n + '0');
 }
