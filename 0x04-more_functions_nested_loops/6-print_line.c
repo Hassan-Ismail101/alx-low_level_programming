836400b5d04ce8ed4e11c8405f8192ea8042c685
Hassan Ismail <hassannasr6218@gmail.com>
2023-03-23 04:46:58 -0700

    print line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_line - print @n times of line
+ * @n: number of lines should printed
+ */
+
+void print_line(int n)
+{
+	while (n)
+	{
+		_putchar('_');
+		ch--;
+	}
+	_putchar('\n');
+}
