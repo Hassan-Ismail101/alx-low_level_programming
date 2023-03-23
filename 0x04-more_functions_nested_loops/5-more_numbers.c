549f183fc96a034ded33b58208ac159941717102
Hassan Ismail <hassannasr6218@gmail.com>
2023-03-23 05:02:38 -0700

    more number

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * most_numbers - print the numbers from '0' to '14'
+ */
+
+void print_most_numbers(void)
+{
+	char counter;
+	char ch;
+
+	counter = 0;
+	ch = 48;
+	for (counter = 1; counter <= 15; counter++)
+	{
+		if (counter / 10)
+			_putchar('1');
+		_putchar(ch);
+		ch++;
+		if (ch == 58)
+			ch = 48;
+	}
+	_putchar('\n');
+}
