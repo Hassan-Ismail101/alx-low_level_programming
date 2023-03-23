35e0eed8d4ffcbbf28b71a9ebceacf099c0da0a1
Hassan Ismail <hassannasr6218@gmail.com>
2023-03-23 05:37:20 -0700

    num 7

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_diagonal - print diagonal type of '\'
+ * @n: the number of counts should printed
+ */
+
+void print_diagonal(int n)
+{
+	int space;
+	int counter;
+
+	for (counter = 1; counter <= n; counter++)
+	{
+		for(space = 1; space < counter; space++)
+			_putchar(' ');
+		_putchar('\\');
+		_putchar('\n');
+	}
+}
