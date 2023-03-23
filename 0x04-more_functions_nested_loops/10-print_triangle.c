c0e35fd360f35257311d80c74c8a73dc4756e16e
Hassan Ismail <hassannasr6218@gmail.com>
2023-03-23 13:36:16 -0700

    triangle

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -1,20 +1,25 @@
 #include "main.h"
 /**
- * print_triangel - prints shape of triangle
+ * print_triangle - prints shape of triangle
  * @size: Number of triangle
  */
-void print_triangel(int size)
+void print_triangle(int size)
 {
-	int dash;
-	int counter;
-	int distance;
-
-	for (counter = 1; counter <= size; counter++)
+	if (size > 0)
 	{
-		for (distance = 1; distance <= size - 1; distance++)
-			_putchar(' ');
-		for (dash = 1; dash <= counter; dash++)
-			_putchar('#');
-		_putchar('\n');
+		int dash;
+		int counter;
+		int distance;
+
+		for (counter = 1; counter <= size; counter++)
+		{
+			for (distance = 1; distance <= size - counter; distance++)
+				_putchar(' ');
+			for (dash = 1; dash <= counter; dash++)
+				_putchar('#');
+			_putchar('\n');
+		}
 	}
+	else
+		_putchar('\n');
 }
