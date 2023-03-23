df12398f7dec25ce46be3d5b1832dcfbb14bd737
Hassan Ismail <hassannasr6218@gmail.com>
2023-03-23 14:10:40 -0700

    adv2

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,35 @@
+#include "main.h"
+/**
+ * print_number - print the number with _putchar
+ * @n: the number should be printed
+ */
+
+void print_number(int n)
+{
+	if (n != 0)
+	{
+		int flag;
+		int part = 1000000000;
+
+		flag = 0;
+		if (n < 0)
+		{
+			_putchar('-');
+			n *= -1;
+		}
+		while (part)
+		{
+			if (n / part && flag != 1)
+				flag = 1;
+			if (flag)
+			{
+				_putchar(n / part + '0');
+			}
+			n %= part;
+			part /= 10;
+		}
+	}
+	else
+		_putchar('0');
+	_putchar('\n');
+}
