6dcecb24516ee26d9207d7553f9cb9a1cfca8e69
Hassan Ismail <hassannasr6218@gmail.com>
2023-03-27 13:20:49 -0700

    adv1

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * _atoi - convert str to number
+ * @s: pointer to the string
+ *
+ * Return: the number value
+ */
+
+int _atoi(char *s)
+{
+	int flag;
+	int num;
+	int mines;
+	int itr;
+
+	flag = 0;
+	num = 0;
+	mines = 0;
+	itr = 0;
+	while (*(s + itr) != '\0')
+	{
+		if (flag == 0 && *(s + itr) == '-')
+			mines++;
+		if (*(s + itr) >= '0' && *(s + itr) <= '9')
+		{
+			num = (num * 10) + (*(s + itr) - 48);
+			flag = 1;
+		}
+		if (flag == 1 && (*(s + itr) < '0' || *(s + itr) > '9'))
+			break;
+		itr++;
+	}
+	if (mines % 2 != 0)
+		return (-1 * num);
+	return (num);
+}
