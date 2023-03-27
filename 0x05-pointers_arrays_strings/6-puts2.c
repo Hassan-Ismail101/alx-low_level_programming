e5bd7a235260a099f5b8c6901932f2998d4ed5a0
Hassan Ismail <hassannasr6218@gmail.com>
2023-03-27 12:25:04 -0700

    half the lies

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -10,9 +10,12 @@ void puts2(char *str)
 	int counter;
 
 	counter = 0;
-	while ((*(str + counter) != '\0') && (*(str + counter - 1) != '\0'))
+	while (*(str + counter) != '\0')
 	{
 		_putchar(*(str + counter));
+		if (*(str + counter) == '\0')
+			break;
 		counter += 2;
 	}
+	_putchar('\n');
 }
