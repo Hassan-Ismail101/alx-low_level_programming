c8a7c625633b03a8833f30aa8d3be66fa922a273
Hassan Ismail <hassannasr6218@gmail.com>
2023-03-30 06:26:10 -0700

    adv5

---
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -0,0 +1,46 @@
+#include "main.h"
+#include <ctype.h>
+
+/**
+ * print_buffer - print bytes of buffer
+ * @b: the pointer to the buffer
+ * @size: the size of the string
+ */
+
+void print_buffer(char *b, int size)
+{
+	int itr;
+	int loop;
+
+	itr = 0;
+	loop = 0;
+	//printf("%p\n", b);
+	while (itr < size)
+	{
+		printf("%08x: ", itr);
+		for (loop = 0; loop < 10; loop += 2)
+		{
+			if (itr + loop > size - 1)
+			{
+				printf("%4s ", " ");
+				continue;
+			}
+			printf("%02x", *(b + itr + loop));
+			printf("%02x ", *(b + itr + loop + 1));
+		}
+		for (loop = 0; loop < 10; loop++)
+                {
+                        if (itr + loop > size - 1)
+     				break;
+			if (isprint(*(b + itr + loop)))
+				printf("%c",*(b + itr + loop));
+			else
+				printf(".");
+
+                }
+
+		printf("\n");
+		itr += 10;
+					
+	}
+}
--- 0x06-pointers_arrays_strings/source.c
+++ 0x06-pointers_arrays_strings/source.c
@@ -3,27 +3,39 @@
 
 int main(void)
 {
+	/*
 	char *n = "1234567892434574367823574575678477685785645685876876774586734734563456453743756756784458";
 	char *m = "9034790663470697234682914569346259634958693246597324659762347956349265983465962349569346";
 	char *a = "1";
 	char *b = "90000";
 	char r[100];
-	char r2[10];
-	char r3[11];
+	char r2[2];
+	char r3[3];
 	char *res;
 	
-	res = infinite_add(a, b, r, 5);
+	res = infinite_add(n, m, r, 100);
 	if (res == 0)
 	{
 		printf("Error\n");
 	}
 	else
 	{
-		printf("%s + %s = %s\n", a, b, res);
+		printf("%s + %s = %s\n", n, m, res);
 	}
+	n = "01";
+	m = "10";
+	res = infinite_add(n, m, r2, 2);
+        if (res == 0)
+        {
+                printf("Error\n");
+        }
+        else
+        {
+                printf("%s + %s = %s\n", n, m, res);
+        }
 	n = "0";
-	m = "1";
-	res = infinite_add(n, m, r, 2);
+        m = "0";
+        res = infinite_add(n, m, r2, 2);
         if (res == 0)
         {
                 printf("Error\n");
@@ -32,9 +44,7 @@ int main(void)
         {
                 printf("%s + %s = %s\n", n, m, res);
         }
-	n = "99";
-        m = "1";
-        res = infinite_add(n, m, r, 2);
+	res = infinite_add(n, m, r3, 3);
         if (res == 0)
         {
                 printf("Error\n");
@@ -43,14 +53,10 @@ int main(void)
         {
                 printf("%s + %s = %s\n", n, m, res);
         }
-	res = infinite_add(n, m, r, 3);
-        if (res == 0)
-        {
-                printf("Error\n");
-        }
-        else
-        {
-                printf("%s + %s = %s\n", n, m, res);
-        }
+	*/
+	char buffer[] = "This is a string!\0And this is the rest of the #buffer :)\1\2\3\4\5\6\7#cisfun\n\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\x20\x21\x34\x56#pointersarefun #infernumisfunhassan\n";
+	printf("%s\n", buffer);
+	printf("-----------------------\n");
+	print_buffer(buffer , sizeof(buffer));
 	return (0);
 }
