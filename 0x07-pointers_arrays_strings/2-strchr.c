90a0d768942291c8ab68522767dea224f6a7c75a
Hassan Ismail <hassannasr6218@gmail.com>
2023-04-03 15:44:33 -0700

    num 2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _strchr - return the first occurrence of the char @c
+ * @s: the string to search in
+ * @c: the char to search for
+ * Return: the pointer to the first occurrence of the @c in the s
+ */
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+			return (s);
+		s++;
+	}
+	return (NULL);
+}
--- 0x07-pointers_arrays_strings/source.c
+++ 0x07-pointers_arrays_strings/source.c
@@ -21,6 +21,7 @@ void simple_print_buffer(char *buffer, unsigned int size)
 }
 int main(void)
 {
+	/*
 	char buffer[98] = {0x00};
 	char buffer2[98] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14};
 
@@ -28,5 +29,14 @@ int main(void)
 	_memcpy(buffer + 50, buffer2, 10);
 	printf("---------------------------------------------\n");
 	simple_print_buffer(buffer, 98);
+	*/
+	char *s = "hello";
+	char *f;
+
+	f = _strchr(s, 'l');
+	if (f != NULL)
+	{
+		printf("%s\n", f);
+	}
 	return (0);
 }
