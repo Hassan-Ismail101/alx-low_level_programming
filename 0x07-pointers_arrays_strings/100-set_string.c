83f83acfd7f44184421da9fafc23d82bc278658e
Hassan Ismail <hassannasr6218@gmail.com>
2023-04-03 20:16:25 -0700

    and now'

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * set_string - set a string @s into string @to
+ * @s: the source string
+ * @to: the destination string
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
--- 0x07-pointers_arrays_strings/source.c
+++ 0x07-pointers_arrays_strings/source.c
@@ -22,14 +22,6 @@ void simple_print_buffer(char *buffer, unsigned int size)
 int main(void)
 {
 	/*
-	char buffer[98] = {0x00};
-	char buffer2[98] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14};
-
-	simple_print_buffer(buffer, 98);
-	_memcpy(buffer + 50, buffer2, 10);
-	printf("---------------------------------------------\n");
-	simple_print_buffer(buffer, 98);
-	*/
 	int c3[3][3] = {
 		{0, 1, 5},
 		{10, 11, 12},
@@ -37,6 +29,11 @@ int main(void)
 	};
 	int c1[1][1] = {{1}};
 	print_diagsums((int *)c3, 3);
-	print_diagsums((int *)c1, 1);
+	print_diagsums((int *)c1, 1);*/
+	char *s0 = "Bob Dylan";
+	char *s1 = "Robert Allen";
+	printf("%s, %s\n", s0, s1);
+	set_string(&s1, s0);
+	printf("%s, %s\n", s0, s1);
 	return (0);
 }
