60ed3f680c5ce54aea6076c7f7a7a0ae592e0a9d
Hassan Ismail <hassannasr6218@gmail.com>
2023-04-04 05:25:57 -0700

    prime for large

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,4 +1,37 @@
 #include <stdio.h>
+int root(double n)
+{
+	double low;
+	double high;
+	double mid;
+	int counter;
+
+	if (n > 1)
+	{
+		low = 1;
+		high = n;
+	}
+	else
+	{
+		low = n;
+		high = 1;
+	}
+	while (100 * low * low < n)
+		low *= 10;
+	while (0.01 * high * high > n)
+		high *= 0.1;
+	for (counter = 0; counter < 100; counter++)
+	{
+		mid = (low + high) / 2;
+		if ((mid * mid) == n)
+			return (mid);
+		if ((mid * mid) > n)
+			high = mid;
+		else
+			low = mid;
+	}
+	return ((int) mid + 1);
+}
 /**
  * prime - check if the number is prime
  * @num: the number to be checked
@@ -12,9 +45,11 @@ int prime(long long num)
 		return (0);
 	int flag;
 	int counter;
+	int limit;
 
+	limit = root(num);
 	flag = 1;
-	for (counter = 2; counter < num / 2; counter++)
+	for (counter = 2; counter <= limit; counter++)
 		if (num % counter == 0)
 			return (0);
 	return (1);
@@ -28,9 +63,11 @@ int main(void)
 {
 	long long num;
 	long long counter;
+	long long start;
 
 	num = 612852475143;
-	for (counter = (num / 2); counter > 2; counter--)
+	start = root(num);
+	for (counter = start; counter < num / 2; counter++)
 	{
 		if (num % counter == 0)
 		{
