2a4f251102bce926a745cc2879ed6ef1988727b9
Hassan Ismail <hassannasr6218@gmail.com>
2023-04-05 08:01:40 -0700

    and now:

---
+++ 0x04-more_functions_nested_loops/testing.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+int main(void)
+{
+	print_number(987654321);
+	_putchar('\n');
+	print_number(10);
+	_putchar('\n');
+	print_number(111);
+	_putchar('\n');
+	print_number(-2324);
+	_putchar('\n');
+	print_number(-103020);
+	_putchar('\n');
+	return (0);
+}
