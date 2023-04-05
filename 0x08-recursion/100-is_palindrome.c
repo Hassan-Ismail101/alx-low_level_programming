8f826b43873b60d8c50113b13f9f9d3919a25f43
Hassan Ismail <hassannasr6218@gmail.com>
2023-04-05 16:16:17 -0700

    palindrome

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,8 +1,36 @@
 #include "main.h"
-
+/**
+ * string_end - get the address of last letter in string
+ * @s: the string
+ * Return: the address of the last char
+ */
+char *string_end(char *s)
+{
+	if (*s == '\0')
+		return (s - 1);
+	string_end(s + 1);
+}
+/**
+ * my_palidrome - check if the string is palindrome or not
+ * @start: pointer to the start of the string
+ * @end: pointer to the last of the string
+ * Return: True if the string is palindrome and false if not
+ */
+int my_palidrome(char *start, char *end)
+{
+	if (*start == '\0')
+		return (1);
+	return ((*start == *end) && my_palidrome(start + 1, end - 1));
+}
+/**
+ * is_palindrome - check if the string is palindrome or not
+ * @s: The string to be checked
+ * Return: True if the string is palindrome and false if not
+ */
 int is_palindrome(char *s)
 {
-	char *w;
+	char *e;
 
-	w = s;
-
+	e = string_end(s);
+	return (my_palidrome(s, e));
+}
