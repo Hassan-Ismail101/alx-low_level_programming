2f063880f67d3919fc0ea75f02d5d419484f3a25
Hassan Ismail <hassannasr6218@gmail.com>
2023-04-05 19:38:26 -0700

    with requirements

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * wildcmp - compare between two string if they are similar or not
+ * @s1: the first string
+ * @s2: the second string
+ * Return: true if the two strings are similar and false if not
+ */
+int wildcmp(char *s1, char *s2)
+{
+
+}
