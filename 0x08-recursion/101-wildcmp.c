45fd814d622fe4c4518d8f96fc2996fc737039be
Hassan Ismail <hassannasr6218@gmail.com>
2023-04-05 19:50:45 -0700

    wildcmp

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -7,7 +7,14 @@
  */
 int wildcmp(char *s1, char *s2)
 {
+	if (*s1 == *s2 && *s1 == '\0')
+		return (1);
+	if (*s1 == '*' && *(s1 + 1) != *s2)
+		wildcmp(s1, s2 + 1);
+	if (*s1 == '*' && *(s1 + 1) == *s2)
+                wildcmp(s1 + 2, s2 + 1);
+	if (*s1 == *s2)
+		wildcmp(s1 + 1, s2 + 1);
 	if (*s1 != *s2)
 		return (0);
-	return (1);
 }
