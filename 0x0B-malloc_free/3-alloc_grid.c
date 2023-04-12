39bdad0a31c91e445dabb9a64f9f642eac44928a
Hassan Ismail <hassannasr6218@gmail.com>
2023-04-11 18:51:42 -0700

    and now

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <stdlib.h>
 /**
- * alloc_grid - 
+ * alloc_grid - create 2D array
  * @width: 1D size
  * @height: 2D size
  * Return: pointer of 2D array
@@ -11,16 +11,18 @@ int **alloc_grid(int width, int height)
 	int **ptr;
 	int itr1D;
 	int itr2D;
+
 	if (width <= 0 || height <= 0)
 		return (NULL);
-
 	itr1D = 0;
 	itr2D = 0;
-	ptr = (int **)malloc(sizeof(int*) * height);
+	ptr = (int **)malloc(sizeof(int *) * height);
 	if (ptr == NULL)
 		return (NULL);
 	for (itr1D = 0; itr1D < width; itr1D++)
-		*(ptr + itr1D) = malloc(sizeof(int) * height);
+		*(ptr + itr1D) = (int *)malloc(sizeof(int) * height);
+	if (ptr == NULL)
+		return (NULL);
 	for (itr1D = 0; itr1D < width; itr1D++)
 		for (itr2D = 0; itr2D < height; itr2D++)
 			ptr[itr1D][itr2D] = 0;
--- 0x0B-malloc_free/source.c
+++ 0x0B-malloc_free/source.c
@@ -41,10 +41,10 @@ int main(int ac, char *av[])
 	int **grid;
 
 	printf("########### number 3 #############\n");
-	grid = alloc_grid(6, 4);
+	grid = alloc_grid(48, 32);
 	if (grid == NULL)
 		return (1);
-	print_grid(grid, 6, 4);
+	print_grid(grid, 48, 32);
 	printf("\n");
 	grid[0][3] = 98;
 	grid[3][4] = 402;
