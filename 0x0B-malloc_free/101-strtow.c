886df05a645439778c90d424b23c85ae1a547ae1
Hassan Ismail <hassannasr6218@gmail.com>
2023-04-11 19:56:38 -0700

    just increase

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * strtow - turn string into word
+ * @str: the string
+ * Return: pointer to the word
+ */
+char **strtow(char *str)
+{
+	char **ptr;
+	
+	ptr = NULL;
+	return(NULL);
+}
---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * free_grid - free the 2D array
+ * @grid: the 2D array
+ * @height: the 2D dimension
+ */
+void free_grid(int **grid, int height)
+{
+
+}
