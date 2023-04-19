292ec209f1ceb439f9a3f54d3afc9d26d516956d
Hassan Ismail <hassannasr6218@gmail.com>
2023-04-19 07:51:55 -0700

    returned back

---
+++ 0x0F-function_pointer/0-print_name.c
@@ -0,0 +1,11 @@
+#include "function_pointers.h"
+/**
+ * print_name - function used to print a string based on way of
+ * pointer of function
+ * @name: The string which should be printed
+ * @f: the method to print that string
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
---
+++ 0x0F-function_pointer/function_pointers.h
@@ -0,0 +1,5 @@
+#ifndef FUNCTION_H
+#define FUNCTION_H
+void print_name(char *name, void (*f)(char *));
+int _putchar(char c);
+#endif
