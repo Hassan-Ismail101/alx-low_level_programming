a3004da23afae10b849d22a71605a072b28b82b7
Hassan Ismail <hassannasr6218@gmail.com>
2023-04-19 08:45:40 -0700

    secho

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+#include <string.h>
+
+/**
+ * init_dog - initialize the members of struct
+ * @d: the struct
+ * @name: the name of the dog
+ * @age: the age of the dog
+ * @owner: the name of dog'owner
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	strcpy(d->name, name);
+	d->age = age;
+	strcpy(d->owner, owner);
+}
