e2bf1e09b8946b35ffcc9460de406243b1dcb057
Hassan Ismail <hassannasr6218@gmail.com>
2023-04-19 08:38:09 -0700

    struct

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - short description
+ * @name: first member of string type
+ * @age: second member of int type
+ * @owner: third member of string type
+ *
+ * Description: info of dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
