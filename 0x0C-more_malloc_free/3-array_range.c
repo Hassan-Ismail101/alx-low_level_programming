79c4cfaa43a6fdcdbe9a4248125b1e2f9282c119
Hassan Ismail <hassannasr6218@gmail.com>
2023-04-23 06:42:21 -0700

    and now

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -13,7 +13,7 @@ int *array_range(int min, int max)
 
 	if (min > max)
 		return (NULL);
-	ptr = malloc(max - min + 1);
+	ptr = malloc((max - min + 1) * sizeof(int));
 	if (ptr == NULL)
 		return (NULL);
 	for (itr = min; itr <= max; itr++)
--- 0x0C-more_malloc_free/source.c
+++ 0x0C-more_malloc_free/source.c
@@ -51,8 +51,8 @@ int main(void)
 	simple_print_buffer(a, 98);
 	*/
 	free(a);
-	b = array_range(1, 1);
-	simple_print_buffer(b, 2);
+	b = array_range(0, 98);
+	simple_print_buffer(b, 99);
 	free(b);
 	return (0);
 }
