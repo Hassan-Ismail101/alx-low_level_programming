5d43325b28bb1bb56a79c5a395ec145656578c83
Hassan Ismail <hassannasr6218@gmail.com>
2023-04-24 17:47:30 -0700

    and Now

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -14,10 +14,10 @@ dog_t *new_dog(char *name, float age, char *owner)
 	char *dynName, *dynOwner;
 	dog_t *ptr;
 
-	dynName = malloc(sizeof(name));
+	dynName = malloc(strlen(name) + 1);
 	if (dynName == NULL)
 		return (NULL);
-	dynOwner = malloc(sizeof(owner));
+	dynOwner = malloc(strlen(owner) + 1);
 	if (dynOwner == NULL)
 		return (NULL);
 	ptr = malloc(sizeof(dog_t));
