16c6a3df97a09521a1d34184b98b66304e7ff7b4
root <root@Voltov.localdomain>
2023-08-04 00:58:07 +0300

    limit

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -8,7 +8,7 @@
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	if (index == sizeof(long int) * 8)
+	if (index >= sizeof(n) * 8)
 		return (-1);
 	return (((1UL << index) & n) >= 1);
 }
