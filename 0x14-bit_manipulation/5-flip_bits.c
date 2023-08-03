1db8d4b0c1b5bc91fe9abb0cec6fa78f44e79bc4
root <root@Voltov.localdomain>
2023-08-04 01:03:01 +0300

    flip

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -8,5 +8,15 @@
  */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
+	unsigned int counter = 0;
+	unsigned long int temp;
 
+	temp = n ^ m;
+	while (temp)
+	{
+		if (1 & temp)
+			counter++;
+		temp >>= 1;
+	}
+	return (counter);
 }
