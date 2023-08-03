59b57ce251f32999bdbf7b73a0d303ab55f1f6b7
root <root@Voltov.localdomain>
2023-08-03 12:48:35 +0300

    betty style

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,17 +8,17 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-    unsigned int result = 0;
+	unsigned int result = 0;
 
-    if (b == NULL)
-        return (0);
-    while (*b != '\0')
-    {
-        if (*b != '0' && *b != '1')
-            return (0);
-        result *= 2;
-        result += (*b - '0');
-        b++;
-    }
-    return (result);
+	if (b == NULL)
+		return (0);
+	while (*b != '\0')
+	{
+		if (*b != '0' && *b != '1')
+			return (0);
+		result *= 2;
+		result += (*b - '0');
+		b++;
+	}
+	return (result);
 }
